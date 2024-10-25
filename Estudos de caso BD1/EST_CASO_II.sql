a7209b81b88d27ff8b1f584f4254e1d97fba836b
underthedarkxx <roberto.diasfurtadoaraujo@gmail.com>
2024-10-24 21:47:52 -0300

    Exercicios de fixação completos

--- Estudos de caso BD1/EST_CASO_II.sql
+++ Estudos de caso BD1/EST_CASO_II.sql
@@ -1,20 +1,25 @@
-CREATE SCHEMA EmpresaPassagemAereas;
+-- Active: 1728688414725@@127.0.0.1@3306@est_caso_ii
+#CREATE SCHEMA Est_Caso_II;
 
 
-use EmpresaPassagemAereas;
+use Est_Caso_II;
 
 CREATE TABLE IF NOT EXISTS VOOS(
     voo_Codigo INT PRIMARY KEY,
+    data_Hora_Origem DATETIME NOT NULL,
+    data_Hora_Destino DATETIME NOT NULL,
     aeroporto_Origem VARCHAR(30) NOT NULL,
     aeroporto_Destino VARCHAR(30) NOT NULL,
-    aeroporto_Prefixo VARCHAR(10) NOT NULL,
-    CONSTRAINT FK_AERONAVES FOREIGN KEY(aeroporto_Prefixo)
-    REFERENCES AERONAVES(aeronave_Prefixo),
-    data_Hora_Origem DATETIME NOT NULL,
-    data_Hora_Destino DATETIME NOT NULL
+    aeronave_FK VARCHAR(10),
+    CONSTRAINT CE_Aeronaves FOREIGN KEY(aeronave_FK)
+    REFERENCES Aeronaves(aeronave_Prefixo)
 );
+#id_pedido INT PRIMARY KEY,
+    #id_cliente VARCHAR(100),
+    #valor DECIMAL(10, 2),
+    #CONSTRAINT fk_cliente FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 
-CREATE TABLE IF NOT EXISTS AERONAVES(
+CREATE TABLE IF NOT EXISTS Aeronaves(
     aeronave_Prefixo VARCHAR(10) PRIMARY KEY,
     aeronave_Modelo VARCHAR(45) NOT NULL,
     aeronave_anofabri VARCHAR(45) NOT NULL,
--- Sala de aula/Exe_fix_I.sql
+++ Sala de aula/Exe_fix_I.sql
@@ -1,2 +1,57 @@
-EXPLAIN CREATE TABLE IF NOT EXISTS seguidores_Usuarios(     FK_usuario INT,     FK_Seguidor INT,     CONSTRAINT usuario_fk FOREIGN KEY (FK_usuario)     REFERENCES Usuario (ID_Usuario),     CONSTRAINT seguidor_fk FOREIGN KEY (FK_Seguidor)     REFERENCES Seguidores(seguidores) )
-EXPLAIN CREATE TABLE IF NOT EXISTS seguidores_Usuarios(     FK_usuario INT,     FK_Seguidor INT,     CONSTRAINT usuario_fk FOREIGN KEY (FK_usuario)     REFERENCES Usuario (ID_Usuario),     CONSTRAINT seguidor_fk FOREIGN KEY (FK_Seguidor)     REFERENCES Seguidores(seguidores) )
+-- Active: 1728688414725@@127.0.0.1@3306@banquinho
+#CREATE SCHEMA Exe_fix
+
+use Exe_fix;
+
+CREATE TABLE IF NOT EXISTS Eventos(
+    ID_Evento INT PRIMARY KEY,
+    nome_Evento VARCHAR(100) NOT NULL,
+    data_Evento DATE,
+    entidade_Evento VARCHAR(100) NOT NULL,
+    local_Evento INT
+);
+
+CREATE TABLE IF NOT Exists Locais(
+    id_Local INT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    log_Local_ VARCHAR(100),
+    numlog_Local INT,
+    bairro_Local VARCHAR(100),
+    cidade VARCHAR(100),
+    UF VARCHAR(2)
+)
+
+CREATE TABLE IF NOT EXISTS Atividades(
+    id_Atividade INT PRIMARY KEY,
+    nome_Atv VARCHAR(100) NOT NULL,
+    data_Atv DATE NOT NULL,
+    hora_Atv TIME NOT NULL,
+    evento_Atv INT 
+);
+
+CREATE TABLE IF NOT EXISTS Participantes(
+    num_Inscricao INT PRIMARY KEY,
+    #CPF VARCHAR(11),
+    nome VARCHAR(100)NOT NULL,
+    data_Nasc DATE NOT NULL,
+    empresa VARCHAR(100)
+);
+#perguntar ao gustavo como essa tabela tem 2 chaves primarias
+
+CREATE TABLE IF NOT EXISTS Participantes_Eventos(
+    PE_Inscricao INT,
+    ID_Evento INT,
+    CONSTRAINT FK_Participantes FOREIGN KEY(PE_Inscricao)
+    REFERENCES Participantes (num_Inscricao),
+    CONSTRAINT FK_Eventos FOREIGN KEY(ID_Evento)
+    REFERENCES Eventos (ID_Evento)
+);
+
+CREATE TABLE IF NOT EXISTS Participantes_Atividades(
+    PA_Participantes INT,
+    PA_Atividades INT,
+    CONSTRAINT FK_Participante FOREIGN KEY(PA_Participantes)
+    REFERENCES Participantes (num_Inscricao),
+    CONSTRAINT FK_Atividades FOREIGN KEY(PA_Atividades)
+    REFERENCES Atividades(id_Atividade)
+);
