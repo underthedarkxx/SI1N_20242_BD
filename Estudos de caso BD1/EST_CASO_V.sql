62b518c177a52b8c7cd53aabe5adc8ec6959fed8
underthedarkxx <roberto.diasfurtadoaraujo@gmail.com>
2024-11-02 15:51:42 -0300

    Est caso V completo

--- Estudos de caso BD1/EST_CASO_V.sql
+++ Estudos de caso BD1/EST_CASO_V.sql
@@ -47,3 +47,38 @@ CREATE TABLE IF NOT EXISTS Fornecedores(
     CONSTRAINT FK_Prod_Forn FOREIGN KEY (Cod_Prod)
     REFERENCES Produtos(Cod_Prod)
 );
+
+CREATE TABLE IF NOT EXISTS Enderecos(
+    ID_Enderecos INT PRIMARY KEY,
+    Num_End INT NOT NULL,
+    Tipo_End VARCHAR(20) NOT NULL,
+    Logradouro_End VARCHAR(50) NOT NULL,
+    Complemento VARCHAR(100),
+    CEP_End INT NOT NULL,
+    Bairro_End VARCHAR(50) NOT NULL,
+    Estado_End VARCHAR(30) NOT NULL,
+    Cidade_End VARCHAR(50) NOT NULL,
+    CPF VARCHAR(11),
+    CONSTRAINT FK_Clientes_End FOREIGN KEY(CPF)
+    REFERENCES Clientes(CPF)
+    );
+
+CREATE TABLE IF NOT EXISTS Clientes_Produtos(
+    CPF VARCHAR(11),
+    Cod_Prod VARCHAR(30),
+    PRIMARY KEY(CPF, Cod_Prod),
+    CONSTRAINT FK_Clientes_Produtos FOREIGN KEY(CPF)
+    REFERENCES Clientes(CPF),
+    CONSTRAINT FK_Produtos_Clientes FOREIGN KEY (Cod_Prod)
+    REFERENCES Produtos(Cod_Prod)
+);
+
+CREATE TABLE IF NOT EXISTS Vendas_Produtos(
+    Cod_Venda VARCHAR(50),
+    Cod_Prod VARCHAR(30),
+    PRIMARY KEY(Cod_Venda, Cod_Prod),
+    CONSTRAINT FK_Vendas_Produtos FOREIGN KEY(Cod_Venda)
+    REFERENCES Vendas(Cod_Venda),
+    CONSTRAINT FK_Produtos_Vendas FOREIGN KEY (Cod_Prod)
+    REFERENCES Produtos(Cod_Prod)
+);
